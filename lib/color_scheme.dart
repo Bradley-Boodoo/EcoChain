06decd87a5d457bd42fb939268fd1e8dd64c1243
bradleyjesseb <bradleyjesseb@gmail.com>
2024-03-30 15:18:01 +0000

    themes

--- lib/color_scheme.dart
+++ lib/color_scheme.dart
@@ -12,6 +12,6 @@ final colorSchemeDark = const ColorScheme.dark().copyWith(
   primary: const Color.fromARGB(150, 0, 248, 236),
   secondary: const Color.fromARGB(255, 2, 61, 58),
   tertiary: Colors.pink,
-  background: const Color.fromARGB(232, 1, 82, 78),
+  background: Color.fromARGB(255, 1, 82, 78),
   surface: const Color(0xFFD0B1F5),
 );
--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       themeMode: ThemeMode.system,
       theme: EcoTheme.lightTheme,
       darkTheme: EcoTheme.darkTheme,
