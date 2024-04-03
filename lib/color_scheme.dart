5b2242d30eb799748ed42754423aa9f0a5cb7134
bradleyjesseb <bradleyjesseb@gmail.com>
2024-04-03 15:05:47 +0000

    updated colors

--- lib/color_scheme.dart
+++ lib/color_scheme.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
 
 final colorSchemeLight = const ColorScheme.light().copyWith(
-  primary: const Color.fromARGB(150, 0, 248, 236),
+  primary: Colors.purple, //const Color.fromARGB(150, 0, 248, 236),
   secondary: const Color.fromARGB(232, 1, 82, 78),
-  tertiary: const Color.fromARGB(136, 0, 248, 170),
+  tertiary: Colors.pink, //const Color.fromARGB(136, 0, 248, 170),
   background: const Color.fromARGB(255, 198, 231, 220),
   surface: const Color(0xFFD0B1F5),
 );
 
 final colorSchemeDark = const ColorScheme.dark().copyWith(
-  primary: const Color.fromARGB(150, 0, 248, 236),
+  primary: Colors.purple,
   secondary: const Color.fromARGB(255, 2, 61, 58),
   tertiary: Colors.pink,
   background: const Color.fromARGB(255, 1, 82, 78),
-  surface: const Color(0xFFD0B1F5),
+  surface: const Color.fromARGB(136, 0, 248, 170),
 );
